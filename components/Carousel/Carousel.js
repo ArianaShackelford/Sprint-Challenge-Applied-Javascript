abc042eca3e46c36c068a4741dd0af6120e7870d
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-01 13:55:13 -0400

    working on the carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,43 @@
     <div class="right-button"> > </div>
   </div>
 */
+const images = ['/assets/carousel/mountains.jpeg', '/assets/carousel/computer.jpeg', '/assets/carousel/trees.jpeg',' /assets/carousel/turntable.jpeg']
+
+const carouselCont = document.querySelector('.carousel-container');
+// images.forEach(image => {
+  const newCarousel = createCarousel(images)
+  carouselCont.append(newCarousel);
+  
+// })
+
+function createCarousel(images){
+
+  const carousel = document.createElement('div');
+  const leftBtn = document.createElement('div');
+  const img1 = document.createElement('img');
+  const img2 = document.createElement('img');
+  const img3 = document.createElement('img');
+  const img4 = document.createElement('img');
+  const rightBtn = document.createElement('div');
+
+  // append
+
+  carousel.append(leftBtn,img1,img2,img3, img4, rightBtn)
+
+  //classes
+  carousel.classList.add('carousel');
+  leftBtn.classList.add('left-button');
+  rightBtn.classList.add('right-button');
+
+  //content
+  // leftBtn.textContent = '◀';
+  img1.setAttribute('src', images[0]);
+  img2.setAttribute('src', images[1]);
+  img3.setAttribute('src', images[2]);
+  img4.setAttribute('src', images[3]);
+  // rightBtn.textContent = '▶';
+
+  return carousel;
+}
+
+console.log('images array',images);
