3be23445fb763d6b2e629d7ef28134c812a894ea
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-01 11:18:40 -0400

    Completed header section

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+console.log(headerContainer);
+
+headerContainer.append(Header());
+function Header() {
+
+    //create elements
+
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    //append elements
+
+    header.append(date, title, temp);
+
+    //classes
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //set content
+    date.textContent = 'SMARCH 28, 2019';
+    title.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    return header;
+}
